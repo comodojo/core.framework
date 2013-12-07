9bde2f3fef582491bbbfa0b935a8144c21161212
comodojo <devel@comodojo.org>
2013-12-08 00:22:24 +0100

    events app improved

--- applications/events/events.js
+++ applications/events/events.js
@@ -10,6 +10,7 @@
 
 $d.require("dijit.form.Button");
 $d.require("dojo.request");
+$d.require("dojo.aspect");
 $d.require("comodojo.Layout");
 $d.require("comodojo.KernelStore");
 $d.require("gridx.modules.SingleSort");
@@ -18,6 +19,8 @@ $d.require("gridx.modules.pagination.PaginationBar");
 $d.require("gridx.modules.Filter");
 $d.require("gridx.modules.filter.FilterBar");
 $d.require("gridx.modules.RowHeader");
+$d.require("gridx.modules.HiddenColumns");
+$d.require("gridx.modules.extendedSelect.Row");
 $d.require("gridx.modules.Menu");
 $d.require("dijit.Menu");
 $d.require("dijit.MenuItem");
@@ -60,8 +63,54 @@ $c.App.load("events",
 					myself.filterByNotPattern();
 				}
 			});
+			this.showEventDetails = new dijit.MenuItem({
+				label: myself.getLocalizedMessage('0014'),
+				onClick: function() {
+					myself.eventDetail();
+				}
+			});
+			//this.followSession = new dijit.MenuItem({
+			//	label: myself.getLocalizedMessage('0015'),
+			//	onClick: function() {
+			//		myself.followSessionId();
+			//	}
+			//});
 			this.eventsMenu.addChild(this.filterByValue);
 			this.eventsMenu.addChild(this.filterByNotValue);
+			this.eventsMenu.addChild(this.showEventDetails);
+			//this.eventsMenu.addChild(this.followSession);
+
+			$d.aspect.before(this.eventsMenu,'_openMyself',function(){
+				var context = myself.container.main.grid.menu.context;
+				var cellData = context.cell.data();
+				var cellName;
+				switch(context.cell.column.id) {
+					case "1":
+						cellName = myself.getLocalizedMessage('0003');
+					break;
+					case "2":
+						cellName = myself.getLocalizedMessage('0001');
+					break;
+					case "3":
+						cellName = myself.getLocalizedMessage('0002');
+					break;
+					case "4":
+						cellName = myself.getLocalizedMessage('0006');
+					break;
+					case "5":
+						cellName = myself.getLocalizedMessage('0004');
+					break;
+					case "6":
+						cellName = myself.getLocalizedMessage('0005');
+					break;
+					case "7":
+						cellName = myself.getLocalizedMessage('0007');
+					break;
+				}
+				myself.filterByValue.attr('label',myself.getLocalizedMessage('0013')+cellName+' = '+(!cellData ? 'null' : cellData));
+				myself.filterByNotValue.attr('label',myself.getLocalizedMessage('0013')+cellName+' != '+(!cellData ? 'null' : cellData));
+			})
+
 
 			this.container = new $c.Layout({
 				modules: ['Grid'],
@@ -75,29 +124,55 @@ $c.App.load("events",
 					params: {
 						title: this.getLocalizedMessage('0000'),
 						structure: [
-							{ name: this.getLocalizedMessage('0003'), field: 'success', width: '10%', style: function(cell) {
-								return "cursor: pointer; color: #fff; font-weight: bold; text-align: center; background: "+(cell.row.rawData().success == 1 ? "#8c8;" : "#c88;");
-							}, formatter: function(data, rowId) {
-								return '<span href="javascript:;" onClick="$c.App.byPid(\''+pid+'\').eventDetail(\''+rowId+'\')">'+myself.getLocalizedMessage("0012")+'</span>';
+							{ name: this.getLocalizedMessage('0003'), field: 'success', dataType: 'bool', width: '5%', style: function(cell) {
+								var color = cell.row.rawData().success == 1 ? "#8c8;" : "#c88;";
+								return "text-align: center; background: "+color+"; color: "+color+";";
 							}},
 							{ name: this.getLocalizedMessage('0001'), width: '30%', field: 'type', formatter: function(data){
 								return $c.Utils.defined(myself.eventData[data.type]) ? myself.getLocalizedMessage(myself.eventData[data.type].name) : data.type
 							}},
-							{ name: this.getLocalizedMessage('0002'), width: '15%', field: 'referTo'},
-							{ name: this.getLocalizedMessage('0006'), width: '15%', field: 'userName'},
-							{ name: this.getLocalizedMessage('0004'), width: '10%', field: 'date', dataType: 'date'},
-							{ name: this.getLocalizedMessage('0005'), width: '10%', field: 'time', dataType: 'time'},
-							{ name: this.getLocalizedMessage('0007'), width: '10%', field: 'host'}
+							{ name: this.getLocalizedMessage('0002'), width: '12%', field: 'referTo'},
+							{ name: this.getLocalizedMessage('0006'), width: '13%', field: 'userName'},
+							{ name: this.getLocalizedMessage('0004'), width: '10%', field: 'date',
+								dataType: 'date',
+								dateParser: function (value) {
+									return value;
+								},
+								decorator: function (cellData) {
+									var dateString = cellData ? dojo.date.locale.format(new Date(cellData),
+										{"selector": "date", "formatLength": "medium"}) : "";
+									return "" + dateString + "";
+								},
+								useRawData: false
+							},
+							{ name: this.getLocalizedMessage('0005'), width: '10%', field: 'time',
+								dataType: 'time'
+								//timeParser: function (value) {
+								//	return value;
+								//},
+								//decorator: function (cellData) {
+								//	return cellData;
+								//	var dateString = cellData ? dojo.date.locale.format(new Date(cellData),
+								//		{"timePattern": "HHmmss", "selector": "time"/*, "formatLength": "medium"*/}) : "";
+								//	return "" + dateString + "";
+								//},
+								//useRawData: false
+							},
+							{ name: this.getLocalizedMessage('0007'), width: '10%', field: 'host'}//,
+							//{ name: this.getLocalizedMessage('0011'), width: '10%', field: 'sessionId'}
 						],
 						style: 'padding: 0px; margin: 0px !important;',
 						store: this.store,
 						modules: [
 							"gridx/modules/SingleSort",
+							"gridx/modules/RowHeader",
 							"gridx/modules/Pagination",
 							"gridx/modules/pagination/PaginationBar",
 							"gridx/modules/Filter",
 							"gridx/modules/filter/FilterBar",
-							"gridx/modules/Menu"
+							"gridx/modules/Menu",
+							//"gridx/modules/HiddenColumns",
+							"gridx/modules/extendedSelect/Row"
 						]
 					}
 				},{
@@ -115,12 +190,14 @@ $c.App.load("events",
 				selected: false
 			});
 
-			//comodojo._tgrid = this.container.main.grid;
+			//this.container.main.grid.hiddenColumns.add('8');
 
 		};
 
-		this.eventDetail = function(rowId) {
-			var data = this.container.main.grid.model.byId(rowId).rawData;
+		this.eventDetail = function() {
+			var context = this.container.main.grid.menu.context;
+			//console.log(context);
+			var data = this.container.main.grid.model.byId(context.cell.row.id).rawData;
 			var html_table = '<div style="height: 600; width: 700;"><table class="ym-table"><tbody>';
 
 			for (i in data) {
@@ -131,6 +208,17 @@ $c.App.load("events",
 
 			$c.Dialog.modal('Details',html_table,false,false);
 		};
+
+		//this.followSessionId = function() {
+		//	var context = this.container.main.grid.menu.context;
+		//	//console.log(context.cell.column.id);
+		//	this.filterBy({
+		//		colId: 8,
+		//		condition: 'equal',
+		//		type: 'Text',
+		//		value: context.cell.row.data()[8]
+		//	});
+		//};
 
 		this.filterByPattern = function() {
 			var context = this.container.main.grid.menu.context;
@@ -154,10 +242,12 @@ $c.App.load("events",
 				break;
 				case 'time':
 					type = 'Time';
-					dtValue = new Date(context.cell.data());
-					dtValue.setDate(1);
-					dtValue.setMonth(0);
-					dtValue.setFullYear(2000);
+					//dtValue = new Date(context.cell.data());
+					dtFields = context.cell.data().split(":");
+					dtValue = new Date(2000,2,23,dtFields[0],dtFields[1],dtFields[2])
+					//dtValue.setDate(1);
+					//dtValue.setMonth(0);
+					//dtValue.setFullYear(2000);
 					value = dtValue;
 				break;
 				default:
@@ -165,7 +255,7 @@ $c.App.load("events",
 					value = context.cell.data();
 				break;
 			}
-			console.log(value);
+			//console.log(value);
 			this.filterBy({
 				colId: context.cell.column.id,
 				condition: 'equal',

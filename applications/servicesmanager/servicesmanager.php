8e3cc3cb270e0ac34d94b741ffb70cb4dfbfeb21
majio <devel@comodojo.org>
2014-03-26 02:39:21 +0100

    servicesmanager & service.pfp completed

--- applications/servicesmanager/servicesmanager.php
+++ applications/servicesmanager/servicesmanager.php
@@ -18,6 +18,11 @@ class servicesmanager extends application {
 	public function init() {
 		$this->add_application_method('get_services', 'getServices', Array(), 'No description yes, sorry', false);
 		$this->add_application_method('get_service', 'getService', Array("name"), 'No description yes, sorry', false);
+		$this->add_application_method('enable_service', 'enableService', Array("name"), 'No description yes, sorry', false);
+		$this->add_application_method('disable_service', 'disableService', Array("name"), 'No description yes, sorry', false);
+		$this->add_application_method('delete_service', 'deleteService', Array("name"), 'No description yes, sorry', false);
+		$this->add_application_method('new_service', 'newService', Array("name","type","supported_http_methods"), 'No description yes, sorry', false);
+		$this->add_application_method('edit_service', 'editService', Array("name"), 'No description yes, sorry', false);
 	}
 
 	public function getServices($params) {
@@ -35,6 +40,61 @@ class servicesmanager extends application {
 		$service = new services_management();
 		try {
 			$s = $service->get_service($params["name"]);
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+		return $s;
+	}
+
+	public function enableService($params) {
+		$service = new services_management();
+		try {
+			$s = $service->enable_service($params["name"]);
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+		return $s;
+	}
+
+	public function disableService($params) {
+		$service = new services_management();
+		try {
+			$s = $service->disable_service($params["name"]);
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+		return $s;
+	}
+
+	public function deleteService($params) {
+		$service = new services_management();
+		try {
+			$s = $service->delete_service($params["name"]);
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+		return $params["name"];
+	}
+
+	public function newService($params) {
+		$service = new services_management();
+		try {
+			$s = $service->new_service($params);
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+		return $s;
+	}
+
+	public function editService($params) {
+		$service = new services_management();
+		try {
+			$s = $service->edit_service($params);
 		}
 		catch (Exception $e) {
 			throw $e;
